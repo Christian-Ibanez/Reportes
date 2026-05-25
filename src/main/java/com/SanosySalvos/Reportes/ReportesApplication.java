06230d86433b5c6329c8c3d575e1dd304b01093a
Christian-Ibanez <chr.ibanez@duocuc.cl>
2026-05-25 18:14:55 -0400

    Resilience

--- src/main/java/com/SanosySalvos/Reportes/ReportesApplication.java
+++ src/main/java/com/SanosySalvos/Reportes/ReportesApplication.java
@@ -2,6 +2,8 @@ package com.SanosySalvos.Reportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ReportesApplication {
@@ -9,5 +11,10 @@ public class ReportesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReportesApplication.class, args);
 	}
+
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
--- src/main/java/com/SanosySalvos/Reportes/service/impl/ReporteServiceImpl.java
+++ src/main/java/com/SanosySalvos/Reportes/service/impl/ReporteServiceImpl.java
@@ -6,9 +6,14 @@ import com.SanosySalvos.Reportes.model.EstadoReporte;
 import com.SanosySalvos.Reportes.model.Reporte;
 import com.SanosySalvos.Reportes.repository.ReporteRepository;
 import com.SanosySalvos.Reportes.service.ReporteService;
+import com.SanosySalvos.Reportes.service.UsuarioClient;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -20,14 +25,20 @@ public class ReporteServiceImpl implements ReporteService {
     @Autowired
     private ReporteRepository reporteRepository;
 
+    @Autowired
+    private UsuarioClient usuarioClient;
+    
     @Override
     public ReporteResponseDTO crearReporte(ReporteRequestDTO requestDTO) {
-        // 1. Validaciones de seguridad básicas
         if (requestDTO.getUsuarioId() == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El ID del usuario es obligatorio para crear un reporte.");
         }
 
-        // 2. Mapear los datos de entrada (DTO) hacia la Entidad que entiende la base de datos
+        boolean usuarioValido = usuarioClient.verificarUsuarioExterno(requestDTO.getUsuarioId());
+        if (!usuarioValido) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El usuario indicado no existe en el sistema.");
+        }
+
         Reporte reporte = new Reporte();
         reporte.setUsuarioId(requestDTO.getUsuarioId());
         reporte.setMascotaId(requestDTO.getMascotaId());
@@ -39,21 +50,16 @@ public class ReporteServiceImpl implements ReporteService {
         reporte.setLatitud(requestDTO.getLatitud());
         reporte.setLongitud(requestDTO.getLongitud());
         
-        // Nota: No seteamos el estado ni la fecha de creación porque el @PrePersist de la entidad lo hará solo.
-
-        // 3. Guardar en PostgreSQL
         Reporte reporteGuardado = reporteRepository.save(reporte);
 
-        // 4. Devolver la respuesta formateada
         return mapearAResponseDTO(reporteGuardado);
     }
 
     @Override
     public List<ReporteResponseDTO> obtenerReportesActivos() {
-        // Buscamos solo los que tienen estado ACTIVO
+        
         List<Reporte> reportes = reporteRepository.findByEstado(EstadoReporte.ACTIVO);
         
-        // Convertimos la lista de Entidades a una lista de DTOs usando programación funcional
         return reportes.stream()
                 .map(this::mapearAResponseDTO)
                 .collect(Collectors.toList());
@@ -61,26 +67,19 @@ public class ReporteServiceImpl implements ReporteService {
 
     @Override
     public ReporteResponseDTO marcarComoResuelto(Long reporteId, Long usuarioId) {
-        // 1. Buscamos el reporte, si no existe lanzamos un 404 NOT FOUND
         Reporte reporte = reporteRepository.findById(reporteId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "El reporte no existe."));
 
-        // 2. Validación de seguridad vital: ¿El que intenta cerrar el reporte es el dueño original?
         if (!reporte.getUsuarioId().equals(usuarioId)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Acceso denegado: Solo el creador del reporte puede cerrarlo.");
         }
 
-        // 3. Cambiamos el estado
         reporte.setEstado(EstadoReporte.RESUELTO);
-        
-        // 4. Guardamos los cambios
         Reporte reporteActualizado = reporteRepository.save(reporte);
 
         return mapearAResponseDTO(reporteActualizado);
     }
-
-    // --- MÉTODO AUXILIAR PRIVADO ---
-    // Lo usamos para no repetir código cada vez que necesitamos transformar un Reporte a DTO
+    
     private ReporteResponseDTO mapearAResponseDTO(Reporte reporte) {
         ReporteResponseDTO dto = new ReporteResponseDTO();
         dto.setId(reporte.getId());
